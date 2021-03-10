b52ad4cec6dc7da8c7cfdd4de10288fddc800b39
Irena <irenejpopova6@gmail.com>
2021-03-10 14:32:36 +0100

    dishes added

--- server/seeds/dishes.js
+++ server/seeds/dishes.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+const Dishes = require('../models/Dishes');
+
+const dishes = [
+  {
+    info: {
+      name: 'Beef Rolls',
+      ingrediants: 'beef','onion','tomato sauce',
+      price: 10,
+      photo: '/img/beef_rolls.jpg'
+    },
