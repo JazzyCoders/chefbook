d448ec318b899ddffe81450ef617334694d90114
Irena <irenejpopova6@gmail.com>
2021-03-10 15:27:32 +0100

    files added

---
+++ server/seeds/dishCategories.js
@@ -0,0 +1,15 @@
+userId: String < -> reference
+sections: [
+  {
+    name: "starters",
+    dishes: [
+      dishId -> 93849g8eu8tu8ser7890dg7
+    ],
+  },
+  {
+    name: "grill",
+    dishes: [
+      dishId:
+    ],
+  }
+]
---
+++ server/seeds/orders.js
@@ -0,0 +1,13 @@
+customerId: String
+chefId: String
+dishes: [
+  {
+    dishId: String
+        quantity: Number
+  },
+  {
+    dishId: String
+        quantity: Number
+  }
+],
+  totalPrice: Number
