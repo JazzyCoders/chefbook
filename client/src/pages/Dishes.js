e75138c551b9255621defc092f5b6e8a2b93878a
Irena <irenejpopova6@gmail.com>
2021-03-11 10:19:57 +0100

    crating the empty pages

---
+++ client/src/pages/Dishes.js
@@ -0,0 +1,5 @@
+import react from 'react';
+
+import { Dishes } from '../modules/Dishes.js';
+
+
--- server/seeds/orders.js
+++ server/seeds/orders.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('../seeds/Orders');
 customerId: String
 chefId: String
 dishes: [
