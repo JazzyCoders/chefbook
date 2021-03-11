fb257f070fd5c2380adee7a7948e70710568aacb
Irena <irenejpopova6@gmail.com>
2021-03-11 19:05:07 +0100

    MenuList

---
+++ server/modules/MenuList.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+// todo: to require the data frm the dishes.js in the seeds folder , 
+
+export const Dishes = [
+  {
+    info: {
+      dateAdded: Date
+      name: 'Beef Rolls',
+      ingredients: 'beef', 'onion', 'tomato sauce',
+      price: 10,
+      photo: '/img/beef_rolls.jpg'
+      portions: Number
+    },
+  },
+
+{
+    info: {
+      dateAdded: Date
+      name: 'Beef Rolls',
+      ingredients: 'beef', 'onion', 'tomato sauce',
+      price: 10,
+      photo: '/img/beef_rolls.jpg'
+      portions: Number
+    },
+  },
+
+  {
+    info: {
+      dateAdded: Date
+      name: 'Beef Rolls',
+      ingredients: 'beef', 'onion', 'tomato sauce',
+      price: 10,
+      photo: '/img/beef_rolls.jpg'
+      portions: Number
+    },
+  }
+
+]
