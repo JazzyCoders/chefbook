dc634d078fc686ed4b5f1d76a89bdc485bb14954
Irena <irenejpopova6@gmail.com>
2021-03-11 11:36:57 +0100

    updates

---
+++ client/app.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import React, { Component } from 'react';
+import './App.css';
+
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Nav from './components/Nav';
+/* import Home from './views/Home'; ???? */
