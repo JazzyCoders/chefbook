704c1e0e008c60b0504ed5bf6bea8edaa4b542b3
Irena <irenejpopova6@gmail.com>
2021-03-11 11:36:32 +0100

    loginForm starter

---
+++ client/src/compoinents/LogInForm.js
@@ -0,0 +1,6 @@
+import react from 'react';
+
+import PropTypes from 'prop-types';
+import { Grid, Header, Message } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import LoginForm from './LoginForm';
