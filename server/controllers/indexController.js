a74ae035b1634aff0a6eae2d8b5f6f9d9233c8d4
Irena <irenejpopova6@gmail.com>
2021-03-15 12:59:28 +0100

    indexController

--- server/controllers/indexController.js
+++ server/controllers/indexController.js
@@ -1,1 +1,10 @@
-// TODOS - ADD the controllers
+const path = require("path")
+
+/* NAMED EXPORT */
+exports.index = (req, res, next) => {
+  /*     console.log(__dirname)
+      console.log(path.resolve(__dirname , "../views/index.html")) */
+  console.log(req.query)
+
+  res.sendFile(path.resolve(__dirname, "../build/index.html"))
+}
