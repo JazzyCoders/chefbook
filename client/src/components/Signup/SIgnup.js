da9cc43ef90e71af4852706957c2af21141bf36e
Kinan Naim <kinan.berlin@gmail.com>
2021-03-18 18:50:42 +0100

    Signup Form

--- client/src/components/Signup/SIgnup.js
+++ client/src/components/Signup/SIgnup.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react'
 import './signup.css'
+import axios from 'axios'
+
 export default class Signup extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             username:"",
+             password:"",
+             confirmPassword:"",
+            phone:"",
+            city:""
+        }
+    }
+    
+    changeHandler=(e)=>{
+        this.setState({[e.target.name]:e.target.value})
+    }
+
+    submitHandler=(e)=>{
+        e.preventDefault();
+        console.log(this.state)
+        axios.post()
+    }
+
     render() {
+        const {username,password,confirmPassword,phone,city}=this.state
         return (
             <div className="SignupContainer">
                 <h2>Sign up</h2>
                 <h3>to continue in chefbook</h3>
 
-                <div className="">
-               <input type="text" placeholder="User Name"/>
-               <input type="password" placeholder="Password"/>
-               <input type="password" placeholder="Confirm Password"/>
-               <input type="text" placeholder="Phone"/>
-               <input type="text" placeholder="City"/>
+                <form onSubmit={this.submitHandler}>
+               <input type="text" placeholder="User Name" onChange={this.changeHandler}/>
+               <input type="password" placeholder="Password" onChange={this.changeHandler}/>
+               <input type="password" placeholder="ConfirmPassword" onChange={this.changeHandler}/>
+               <input type="text" placeholder="Phone" onChange={this.changeHandler}/>
+               <input type="text" placeholder="City" onChange={this.changeHandler}/>
                <button type = "submit">Sign up</button>
-
-                </div>                
-
+               </form>
             </div>
         )
     }
