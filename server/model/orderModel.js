9537cba5a5159b0f630191d146e92ffac4cadc2f
Irena <irenejpopova6@gmail.com>
2021-03-18 10:10:58 +0100

    cartModule added, seeds to test

--- server/model/orderModel.js
+++ server/model/orderModel.js
@@ -12,3 +12,22 @@ const OrderSchema = new Schema({
 });
 
 module.exports = mongoose.model("orders", OrderSchema);
+
+
+/* const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  cart: { type: Object, required: true },
+  date: { type: String, required: true },
+  time: { type: String, required: true },
+  preparing: { type: Boolean, required: true },
+  ontheway: { type: Boolean, required: true },
+  delivered: { type: Boolean, required: true },
+  address: { type: String, required: true },
+  name: { type: String, required: true },
+  paymentId: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Order', schema); */
