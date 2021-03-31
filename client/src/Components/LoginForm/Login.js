9f61e52fc927a154aba2f43c9899015a9275c50d
Irena <irenejpopova6@gmail.com>
2021-03-31 16:19:10 +0200

    jstoken generated for user login

--- client/src/Components/LoginForm/Login.js
+++ client/src/Components/LoginForm/Login.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react'
 
-export default class Signin extends Component {
+export default class Login extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    fetch('/server/auth', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(user_token => {
+        let { token } = user_token;
+        localStorage.setItem('token', token);
+        this.props.history.push('/');
+
+      })
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Sign up</h2>
-        <h3>to continue in chefbook</h3>
 
-        <div>
-          <input type="text" value="User Name/>
-                <input type="password" value="Password/>
-                <input type="password" value="Password/>
-                <input type="password" value="Confirm Password>
-                <input type="umber" value="Phone" />
-          <input type="text" value="city" />
-
-
-        </div>
-
-      </div>
+        <div></div>
+        <form onSubmit={this.onSubmit}>
+          <h1>Login</h1>
+          <h3>to continue in chefbook</h3>
+          <input type="email" autoComplete="true" name="email" placeholder="Enter email" value={this.state.email} onChange={this.handleInputChange}></input>
+          <input type="password" autoComplete="true" name="password" placeholder="Enter password" value={this.state.password} onChange={this.handleInputChange}></input>
+          <input type="submit" value="Login" />
+          <input type="button" value="Register" />
+        </form>
     )
   }
 }
+
