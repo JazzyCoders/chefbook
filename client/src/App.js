326fb00b6171bcefab6e128ccd495cb91a70b09c
Kinan Naim <kinan.berlin@gmail.com>
2021-04-01 13:21:47 +0200

    Clean and fix app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect, useContext, createContext } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import DishChefs from './Components/DishChefs'
-import Home from "./Components/Home"
-import HomeUser from './Components/HomeUser'
-import MenuChef from './Components/MenuChef'
-import OffersUser from './Components/OffersUser'
-import OrdersChef from './Components/OrdersChef'
-import OrdersUser from './Components/OrdersUser'
-import ProfileChef from './Components/ProfileChef'
+import DishChefs from './Components/Dish/Section'
+import Home from "./Components/Home/HomeUser"
+import MenuChef from './Components/ChefMenu/MenuChef'
+import OffersUser from './Components/OffersUser/OffersUser'
+import OrdersChef from './Components/Order/Order'
+import ChefProfile from './Components/ChefProfile/ChefProfile'
 import ProfileUser from './Components/ProfileUser'
+import Cart from './Components/Cart/Cart'
 import Section from './components/Dish/Section'
-import Nav from './components/Nav/Nav'
-import Signup from './components/Signup/Signup'
+import Nav from './Components/Nav/Nav'
+import LogOut from './Components/LogOut/Logout'
+import Signup from './Components/Register/Signup'
 import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import Menu from './pages/Menu'
-import Profile from './pages/Profile'
-import Order from './pages/Order'
-import Cart from './pages/Cart'
-import Logout from './pages/Logout'
 import './App.css'
-import OrderComponent from './components/Order/Order/OrderComponent'
-import History from './components/Order/History/History'
+import OrderComponent from './Components/Order/Order/OrderComponent'
+import History from './Components/Order/History/History'
 
 export const MyContext = createContext(null)
 
@@ -45,19 +38,19 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
 
+{/* 
             {/* // user */}
-            <Route path="/user/menu" component={HomeUser} />
+            <Route path="/user/menu" component={Home} />
             <Route path="/user/profile" component={ProfileUser} />
-            <Route path="/user/order" component={OrdersUser} />
             <Route path="/user/dish" component={OffersUser} />
-            <Route path="/logout" component={Logout} />
-
+            <Route path="/logout" component={LogOut} /> 
+ */}
             {/* // chef */}
-            <Route path="/Menu" exact component={Menu} />
-            <Route path="/profile" exact component={Profile} />
-            <Route path="/Order" exact component={Order} />
+            <Route path="/ChefMenu/MenuChef" exact component={MenuChef} />
+            <Route path="/ChefProfile/ChefProfile" exact component={ChefProfile} />
+            <Route path="/Order/Order" exact component={OrdersChef} />
             <Route path="/Cart" exact component={Cart} />
-            <Route path="/Logout" exact component={Logout} />
+            <Route path="/LogOut/Logout" exact component={LogOut} />
           </Switch>
         </Router>
 
