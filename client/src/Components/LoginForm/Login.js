59402f9e709916f684a1c1f09c818ea94a73e21b
Irena <irenejpopova6@gmail.com>
2021-04-14 17:58:06 +0200

    signup & signin form added

--- client/src/Components/LoginForm/Login.js
+++ client/src/Components/LoginForm/Login.js
@@ -1,171 +1,131 @@
-import React, { Component } from 'react'
-import {Link} from "react-router-dom"
-//import Signup from "../Signup/Signup"
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default class Login extends Component {
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light" ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  handleInputChange = (e) => {
-    const { value, name } = e.target;
-    this.setState({
-      [name]: value
-    });
-  }
+export default function SignInSide() {
+  const classes = useStyles();
 
-  /*
-  class Login extends Component {
-  state = {
-    showSpinner: false,
-    successMessage: null,
-    errorMessage: null,
-    username: "",
-    password: ""
-  };
-  
-  handleChange = e => {
-    const key = e.target.name;
-    const value = e.target.value;
-    this.setState({ ...this.state, [key]: value });
-  };
-
-handleSubmit = event => {
-    event.preventDefault();
-    const { username, password } = this.state;
-
-    try {
-      this.setState({ showSpinner: true });
-      logic
-        .login(username, password)
-        .then(() =>
-          this.setState(
-            { showSpinner: false, successMessage: "Welcome!" },
-            () => {
-              setTimeout(() => {
-                this.setState({ successMessage: null });
-                this.props.history.push("/home");
-              }, 2000);
-            }
-          )
-        )
-        .catch(err => this.showError(err));
-    } catch (err) {
-      this.showError(err);
-    }
-  };
-
-showError = err => {
-    this.setState({ showSpinner: false, errorMessage: err.message }, () => {
-      setTimeout(() => {
-        this.setState({ errorMessage: null });
-      }, 2500);
-    });
-  };
-
-  render() {
-    const { handleChange, handleSubmit } = this;
-    const { showSpinner, successMessage, errorMessage } = this.state;
-
-    let message = () => {
-      if (successMessage) return <p className="correct">{successMessage}</p>;
-      else if (errorMessage) return <p className="error">{errorMessage}</p>;
-      return null;
-    };
-
-    return (
-      <div className="login__container">
-        <h1 className="login__title">Sign In</h1>
-        <form className="form-group login__form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <input
-              className="form-control"
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
               required
-              type="text"
-              name={"username"}
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
               autoFocus
-              placeholder="Username"
-              onChange={handleChange}
             />
-          </div>
-
-          <div className="form-group">
-            <input
-              className="form-control"
+            <TextField
+              variant="outlined"
+              margin="normal"
               required
+              fullWidth
+              name="password"
+              label="Password"
               type="password"
-              name={"password"}
-              placeholder="Password"
-              onChange={handleChange}
+              id="password"
+              autoComplete="current-password"
             />
-          </div>
-
-          <div className="form-group">
-            <button className="btn btn-primary btn-lg" type="submit">
-              Login
-            </button>
-            <button
-              className="btn btn-link"
-              href="#"
-              onClick={this.props.onGoBack}
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
             >
-              Go Back
-            </button>
-          </div>
-          {showSpinner ? (
-            <div className="spinner-container">
-              <i class="fa fa-spinner fa-pulse fa-3x fa-fw" />
-            </div>
-          ) : null}
-          {message()}
-        </form>
-      </div>
-    );
-  }
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+            <Box mt={5}>
+              <Copyright />
+            </Box>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
 }
-
-export default withRouter(Login);
-
- */
-  onSubmit = (e) => {
-    e.preventDefault();
-    fetch('http://localhost:5000/users/login', {
-      method: 'POST',
-      body: JSON.stringify(this.state),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(data => {
-        let { token } = data;
-        localStorage.setItem('token', token);
-        this.props.history.push('');
-
-      })
-  }
-
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.onSubmit}>
-          <h1>Login</h1>
-          <h3>to continue in chefbook</h3>
-          <input type="email" autoComplete="true" name="email" placeholder="Enter email" value={this.state.email} onChange={this.handleInputChange}></input>
-          <input type="password" autoComplete="true" name="password" placeholder="Enter password" value={this.state.password} onChange={this.handleInputChange}></input>
-          <button type="submit" value="Login"> Login</button>
-          <Link to={"./signup"} ><button type="button" value="Signup"> SignUp</button></Link>
-          
-        
-        </form>
-      </>
-    )
-  }
-}
-
