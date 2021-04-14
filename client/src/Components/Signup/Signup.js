e0ac2976fb6f1ecf554b9d6e66005476ee668ca8
Irena <irenejpopova6@gmail.com>
2021-04-14 17:47:25 +0200

    add signup form

--- client/src/Components/Signup/Signup.js
+++ client/src/Components/Signup/Signup.js
@@ -1,114 +1,131 @@
-import React, { useState } from 'react'
-import { fade, makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import './signup.module.css'
-import axios from 'axios'
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-const classes = makeStyles();
-export default function Signup() {
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+      </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+        </Typography>
+    );
+}
 
-    const [name, setName] = useState("")
-    const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
-    const [phone, setPhone] = useState("")
-    const [city, setCity] = useState("")
-    const [register, setRegister] = useState(false)
-    const [isChef, setIsChef] = useState(false);
-    const [isCustomer, setIsCustomer] = useState(false);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: "100vh",
+    },
+    image: {
+        backgroundImage: "url(https://source.unsplash.com/random)",
+        backgroundRepeat: "no-repeat",
+        backgroundColor:
+            theme.palette.type === "light" ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        const registerData = {
-            role: isChef ? 'Chef' : 'User',
-            firstName: name,
-            lastName: lastName,
-            email: email,
-            phone: phone,
-            password: password,
-            city: city
-        };
-
-        console.log(registerData)
-
-        axios.post("http://localhost:5000/users/signup", registerData)
-            .then((res) => {
-                console.log(res.data);
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-        setRegister(true)
-    }
+export default function SignInSide() {
+    const classes = useStyles();
 
     return (
-
-        <div className="SignupContainer">
-
-            <h2>Sign up</h2>
-            <h3>to continue in chefbook</h3>
-
-            <form className={classes.root} noValidate autoComplete="off">
-                <Button variant="contained" color="primary">
-                    Submit
-                </Button>
-                <TextField id="standard-basic" label="Standard" />
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            </form>
-
-            <form onSubmit={onSubmit}>
-                <section>
-                    <label htmlFor="asChef">As Chef</label>
-                    <input type="checkbox" onChange={() => setIsChef(!isChef)} name="asChef" className="checkbox" />
-                </section>
-
-                <section>
-                    <label htmlFor="asCustomer">As Customer</label>
-                    <input type="checkbox" onChange={() => setIsChef(!isCustomer)} name="asCustomer" className="checkbox" />
-                </section>
-
-                <div className="input-group-wrap">
-                    <div className="input-group">
-                        <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Name" required />
-                    </div>
-                    <span></span>
-                    <div className="input-group">
-                        <input type="text" onChange={(e) => setLastName(e.target.value)} placeholder="Last name" required />
-                    </div>
-                    <span></span>
-                    <div className="input-group">
-                        <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="email" required />
-                    </div>
-                    <span></span>
-                    <div className="input-group">
-                        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" required />
-                    </div>
-                    <span></span>
-                    <div className="input-group">
-                        <input type="password" onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm password" required />
-                    </div>
-                    <span></span>
-                    <div className="input-group">
-                        <input type="number" onChange={(e) => setPhone(e.target.value)} placeholder="phone" required />
-                    </div>
-                    <span></span>
-                    <div className="input-group">
-                        <input type="city" onChange={(e) => setCity(e.target.value)} placeholder="City" required />
-                    </div>
-                    <span></span>
-
-                    <button type='submit'>Submit</button>
-
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+          </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+            </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="/signup" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
+                    </form>
                 </div>
-            </form>
-
-
-        </div>
-
-
-    )
+            </Grid>
+        </Grid>
+    );
 }
