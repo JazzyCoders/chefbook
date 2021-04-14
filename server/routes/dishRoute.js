1ca69df939daa61695e11cddefae83ff84839011
Irena <irenejpopova6@gmail.com>
2021-04-14 14:25:08 +0200

    dishRoutes

--- server/routes/dishRoute.js
+++ server/routes/dishRoute.js
@@ -1,24 +1,26 @@
 const express = require("express")
 const dishesRoute = express.Router()
-const {getAllDishes,postAddNewDish,putUpdateDish,deleteSingleDish,getSingleDish} = require("../controllers/dishesController")
- const {auth} = require("../middleware/authentication")
+const { getAllDishes, postAddNewDish, putUpdateDish, deleteSingleDish, getSingleDish } = require("../controllers/dishesController")
+const { auth } = require("../middleware/authentication")
 const rules = require("../lib/rulesDishes")
-const Validation = require("../middleware/validation") 
+const Validation = require("../middleware/validation")
 
 /* GET ALL DISHES FROM RESOURCES */
-dishesRoute.get("/", getAllDishes )
+dishesRoute.get("/", getAllDishes)
+
+// GET CHEF DISHES
 
 /* POST REQUEST TO ADD NEW DISH */
-dishesRoute.post("/",Validation(rules),postAddNewDish)
+dishesRoute.post("/", Validation(rules), postAddNewDish)
 
 /* PUT REQUEST TO UPDATE SINGLE Dish IN DATABASE */
-dishesRoute.put("/:id",putUpdateDish )
+dishesRoute.put("/:id", putUpdateDish)
 
 /* DELETE REQUEST TO DELETE SINGLE Dish IN DATABASE */
-dishesRoute.delete("/:id",deleteSingleDish )
+dishesRoute.delete("/:id", deleteSingleDish)
 
 //GET SINGLE Dish FRO  DATABASE
-dishesRoute.get("/:id",getSingleDish)
+dishesRoute.get("/:id", getSingleDish)
 
 
 /* DEFAULT EXPORT */
