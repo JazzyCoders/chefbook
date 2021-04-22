5e174f722452c9cf850b9b0eb32f0f2a1f6a237d
Irena <irenejpopova6@gmail.com>
2021-04-22 12:31:18 +0200

    updates

--- client/src/Components/ChefMenu/MenuChef.js
+++ client/src/Components/ChefMenu/MenuChef.js
@@ -3,35 +3,83 @@ import { MyContext } from "../../App";
 import Nav from "../Nav/Nav";
 
 
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { MyContext } from "../../App";
+import Nav from "../Nav/Nav";
+
+
 export default function MenuChef() {
-  const { dishes,loggedUser } = useContext(MyContext);
+  const { dishes, loggedUser } = useContext(MyContext);
 
   return (
     <div>
-      <Nav/>
-      <h1>WELCOME!! Chef {loggedUser.firstName} </h1>
+      <Nav />
+      <h1>WELCOME! Chef {loggedUser.firstName} </h1>
       <div>
-        <div>
-          <h1>Category</h1>
+        <h1>Category</h1>
+        <div className="BstOff">
+          <h2>Grill</h2>
           <div className="cat1" >
-              {dishes.map((dish)=>{
-                            if (dish.chefId === loggedUser._id) {
-                                return(
-                                  <div className="chefMenuCd" >
-                                    <h2>{dish.name}</h2>
-                                    <hr/>
-                                    <img src={dish.img} alt="image" width="300" height="400" />
-                                    <hr/>
-                                    <button>Edit</button>
-                                    <button>Remove</button>
-                                  </div>
-                                      )
-                                  }
-                              })}
+            {dishes.map((dish) => {
+              if (dish.chefId === loggedUser._id) {
+
+                if (dish.chefId === loggedUser._id && dish.category === "Grill") {
+
+                  return (
+
+                    <div className="OfCard">
+                      <h2>{dish.name}</h2>
+                      <img src={dish.img} alt="image" width="350" height="400" />
+                      <hr />
+                      <div className="btn">
+                        <Link to={"#"}>
+                          <button > Edit </button>
+                        </Link>
+                        <Link to={"#"}>
+                          <button > remove </button>
+                        </Link>
+
+                      </div>
+                    </div>
+
+                  )
+                }
+              }
+            })}
           </div>
-            <div>
-              <button> Add </button> 
-            </div>
+          <div className="AddCat">
+            <button> Add </button>
+          </div>
+        </div>
+        <div className="BstOff">
+          <h2>Vegetarian</h2  >
+
+          {dishes.map((dish) => {
+
+            if (dish.chefId === loggedUser._id && dish.category === "Vegetarian") {
+
+              return (
+
+                <div className="OfCard">
+                  <h2>{dish.name}</h2>
+                  <img src={dish.img} alt="image" width="350" height="400" />
+                  <hr />
+                  <div className="btn">
+                    <Link to={"#"}>
+                      <button> Edit</button>
+                    </Link>
+                    <Link to={"/#"}>
+                      <button >Remove</button>
+                    </Link>
+                  </div>
+                </div>
+              )
+            }
+          })}
+        </div>
+        <div className="AddCat">
+          <button> Add </button>
         </div>
       </div>
     </div>
--- client/src/Components/ChefProUser/ChefProUser.js
+++ client/src/Components/ChefProUser/ChefProUser.js
@@ -1,62 +1,63 @@
-import React, {useState, useEffect,useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { MyContext } from '../../App';
-import styles from './ChefProUser.module.css'
 import NavUser from "../NavUser/NavUser"
+import { useParams } from "react-router-dom"
 
 
-  
-  export default function ChefProUser() {
 
-    const {chefUId} =useContext(MyContext)
-    const [chef, setChef] = useState({})
+export default function ChefProUser() {
+  const { chefId } = useParams();
+  const { chefUId } = useContext(MyContext)
+  const [chef, setChef] = useState({})
 
-   useEffect(() => {
-       fetch(`http://localhost:5000/users/${chefUId}`)
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/${chefId}`)
       .then((res) => res.json())
       .then((result) => {
         console.log(result);
         if (result.success) {
           setChef(result.users);
-          console.log(chef.img)
+          console.log(chef)
         } else {
           console.log(result.message);
         }
       })
-        .catch((err) => console.log(err));
-        }, [])
-       
+      .catch((err) => console.log(err));
+  }, [])
 
-    return (
-      <div>
-        <NavUser/>
-        {chef ?
-          <div className={styles.gridContainer}>
 
-          <h2 className={styles.headingAboutMe}></h2>
-             <h1>Chef: {chef.firstName}  {chef.lastName} </h1> 
-        <div className={styles.gridProfilePic}>
-              <img className={styles.profilePic} src={chef.img} alt="" />
+  return (
+    <div>
+      <NavUser />
+      <div className="chefPr">
+
+        <div className="chefBio">
+          <img src={chef.img} alt="img" />
+          <h1>{chef.firstName}{chef.lastName}</h1>
+          <div className="chefAdr">
+            <h2>Cuisine: {chef.cuisine}</h2>
+            <h2>City: {chef.city}</h2>
+            <h2>Tel: {chef.phone}</h2>
+          </div>
+        </div>
+        <div className="position">
+          <div className="chefInfo">
+            <div className="chefAbt">
+              <h1>About Me</h1>
+              <p>{chef.about} </p>
             </div>
-            <div className={styles.gridProfileData}>
-              <p>Cuisine: {chef.cuisine} | City:{chef.city} |Tel:{chef.phone} </p>
-              </div>
-            
-            <div className={styles.gridAboutMe}>
-              <h2 className={styles.headingAboutMe}>About Me</h2>
-             <p>{chef.about}</p>
-             <hr/>
-             <h2 className={styles.headingAboutMe}>Profile</h2>
-             <p>{chef.description}</p>
+            <div className="chef">
+              <h1>Summary</h1>
+              <p>{chef.description} </p>
             </div>
+          </div>
+          <div className="chefSkills">
+            <h1>Skills</h1>
+            {chef.services}
+          </div>
+        </div>
+      </div>
 
-            <div className={styles.gridSkills}>
-              <h2 className={styles.headingSkills}>Skills</h2>
-              <ul>
-                <li>{chef.services}</li>
-              </ul>
-            </div>
-
-          </div> : ""}
-             </div>        
-    )
+    </div>
+  )
 }
