3d126c0c4f9b90e06799b74f05012279d907c697
Irena <irenejpopova6@gmail.com>
2021-04-22 12:12:14 +0200

    style updates

--- client/src/Components/HomeUser/HomeUser.js
+++ client/src/Components/HomeUser/HomeUser.js
@@ -1,74 +1,71 @@
-import React, { useContext} from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { MyContext } from '../../App';
 import NavUser from '../NavUser/NavUser'
 
 export default function HomeUser() {
-  const {userData,
-        dishes,
-        show, 
-        loggedUser,
-        setChefUId,
-        setShow,
-        chefUId} = useContext(MyContext)
+  const { userData,
+    dishes,
+    show,
+    loggedUser,
+    setChefUId,
+    setShow,
+    chefUId } = useContext(MyContext)
+  //console.log(chefUId);
 
-
-
-    //console.log(chefUId);
-  
   return (
 
     <div >
-      <NavUser/>
+      <NavUser />
       <div className="chefCd">
-        <h1> Welcome  {loggedUser.firstName} </h1> 
-    
-      {userData.map((user) => {
-        if (user.role === "Chef") {
-          return (
-            <div className="chefCard" >
-              <img src={user.img} alt="img" width="400" height="450" />
-              <h2>Name: {user.firstName}</h2>
-              <h3>About Chef </h3>
-              <p>{user.description}</p>
-              <hr/>
-              <div>
-                <Link to={"/ChefProUser"}>
-                  <button onClick={()=> setChefUId(user._id)}  >Profile</button>
-                </Link>
-                <Link to={`/user/offers/${user._id}`}>
-                  <button >Offers</button>
-                </Link>
+        <h1> Welcome  {loggedUser.firstName} </h1>
+
+        {userData.map((user) => {
+          if (user.role === "Chef") {
+            return (
+              <div className="chefCard" >
+                <img src={user.img} alt="img" width="400" height="450" />
+                <h2>Name: {user.firstName}</h2>
+                <h3>About Chef </h3>
+                <p>{user.description}</p>
+                <hr />
+                <div>
+                  <Link to={"/ChefProUser"}>
+                    <button onClick={() => setChefUId(user._id)}  >Profile</button>
+                  </Link>
+                  <Link to={`/user/offers/${user._id}`}>
+                    <button >Offers</button>
+                  </Link>
+                </div>
               </div>
-            </div>
-          )
-        }
-      })}</div>
-      <hr/>
-      
+            )
+          }
+        })}</div>
+      <hr />
+
       <div className="BstDl" >
         <h1>Best Deals</h1>
-        <hr/>
-        {dishes.map((dish)=>{
-          return(
+        <hr />
+        {dishes.map((dish) => {
+          return (
             <div className="BstCd" >
-           <img src={dish.img} alt="images" width="300px" height="300px"/>
-           <h2>${dish.price}</h2>
-           <h3> {dish.name} </h3>
-           <hr/>
-           <div className="btn">
+              <img src={dish.img} alt="images" width="300px" height="300px" />
+              <h2>${dish.price}</h2>
+              <h3> {dish.name} </h3>
+              <hr />
+              <div className="btn">
                 <Link to={"#"}>
-                  <button onClick={()=> setChefUId()}> Rate </button>
+                  <button onClick={() => setChefUId()}> Rate </button>
                 </Link>
                 <Link to={`/dishItem/${dish._id}`}>
                   <button >More</button>
                 </Link>
-           </div>
-        </div>
+              </div>
+            </div>
           )
         })}
-        
-              
+
+
       </div>
     </div>
   )
